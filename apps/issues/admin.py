05fb811cb7749851fb39e40a72f405b6ae2cfaf1
Macktireh <abdimack97@gmail.com>
2024-04-29 23:34:35 +0200

    add issues models and register on the administration site

--- apps/issues/admin.py
+++ apps/issues/admin.py
@@ -1,1 +1,15 @@
-# Register your models here.
+from django.contrib import admin
+from unfold.admin import ModelAdmin
+
+from apps.issues.models import Issue, Tag
+
+
+@admin.register(Issue)
+class IssueAdmin(ModelAdmin):
+    list_display = ("title", "created", "updated")
+
+
+@admin.register(Tag)
+class LabelAdmin(ModelAdmin):
+    list_display = ("name", "color", "description")
+    list_editable = ("color", "description")
